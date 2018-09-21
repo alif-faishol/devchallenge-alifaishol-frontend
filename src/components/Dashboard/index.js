30f2b664359db0de407bd350253eaa603d87276d
Alif Faishol <alifaishol@gmail.com>
2018-09-21 10:24:42 +0700

    base sidebar

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,7 +1,32 @@
 import React from 'react'
 
+import { withStyles } from '@material-ui/core/styles'
+import Drawer from '@material-ui/core/Drawer'
+import Grid from '@material-ui/core/Grid'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemText from '@material-ui/core/ListItemText'
+
 const Dashboard = () => (
-  <div>ini dashboard</div>
+  <Grid
+    container
+  >
+    <Drawer
+      variant="permanent"
+    >
+      <List>
+        <ListItem button>
+          <ListItemText primary="Project" />
+        </ListItem>
+        <ListItem button>
+          <ListItemText primary="Team Performance" />
+        </ListItem>
+        <ListItem button>
+          <ListItemText primary="Calendar" />
+        </ListItem>
+      </List>
+    </Drawer>
+  </Grid>
 )
 
 export default Dashboard
